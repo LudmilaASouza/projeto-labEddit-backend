c9ce662c5f520857b763ebfb27168f12c679ecd6
LudmilaASouza <ludmilasouz.a@hotmail.com>
2023-07-24 23:03:30 -0300

    feat: data modeling

---
+++ src/database/labeddit.sql
@@ -0,0 +1,60 @@
+-- Active: 1690245024507@@127.0.0.1@3306
+CREATE TABLE users (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    nickname TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE posts (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    creator_id TEXT NOT NULL,
+    content TEXT NOT NULL,
+    votes_count INTEGER NOT NULL,
+    comments_count INTEGER NOT NULL,
+    create_at TEXT NOT NULL,
+    FOREIGN KEY (creator_id) REFERENCES users(id)
+            ON UPDATE CASCADE
+            ON DELETE CASCADE
+);
+
+
+CREATE TABLE comments (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    post_id TEXT NOT NULL,
+    creator_id TEXT NOT NULL,
+    content TEXT NOT NULL,
+    votes_count INTERT NOT NULL,
+    create_at TEXT NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES post(id)
+            ON UPDATE CASCADE
+            ON DELETE CASCADE,
+    FOREIGN KEY (creator_id) REFERENCES users(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE posts_votes (
+    post_id TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    vote INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    FOREIGN KEY (post_id) REFERENCES posts(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+
+CREATE TABLE comments_votes (
+    comment_id TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    vote INTEGER NOT NULL,
+    FOREIGN KEY (comment_id) REFERENCES comments(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
